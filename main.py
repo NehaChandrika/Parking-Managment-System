aea0fb2349df2ebddd688e7b0cca8863353902ab
Neha Chandrika <nehadudam21@gmail.com>
2026-01-04 11:02:08 +0530

    Improve input handling for better readability and user experience

--- main.py
+++ main.py
@@ -1,43 +1,69 @@
 parking = []
 print("Welcome")
-a = int(input("Enter the total number of parking slot that are available: "))
+
+a = int(input("Enter the total number of parking slots available: "))
 for k in range(a):
-    parking.append(0)  
+    parking.append(0)
+
+print(
+    "\nEnter 'in' for parking"
+    "\nEnter 'out' for moving out"
+    "\nEnter 'Exit' to Exit"
+)
 
 while True:
-    op = str(input("Enter in for parking\nEnter out for moving out from the parking slot\nEnter exit to Exit the program\n: "))
-    if op == "exit":
+    op = input("\nYour choice: ")
+
+    if op == "Exit":
         print("EXIT")
         break
-    vech = int(input("Please, Enter your vehicle number in terms of natural number only: "))
 
-    if op == "in":
+    elif op == "in":
+        if 0 not in parking:
+            print("The slots are full, Sorry :)")
+            continue
+
+        while True:
+            vehicle = input("Enter your vehicle number (natural number): ")
+            if vehicle.isdigit() and int(vehicle) > 0:
+                vehicle = int(vehicle)
+                break
+            else:
+                print("Enter valid number only")
+
+        if vehicle in parking:
+            print("Vehicle already parked!")
+            continue
+
         for i in range(len(parking)):
             if parking[i] == 0:
-                print("Please park your vehicle at parking slot: ", i + 1)
-                parking[i] = vech
+                print("Please park your vehicle at slot:", i + 1)
+                parking[i] = vehicle
                 break
-        else:
-            print("The slots are full, Sorry :)")
-            
 
     elif op == "out":
+        if all(x == 0 for x in parking):
+            print("No vehicles are parked currently.")
+            continue
+
+        while True:
+            vehicle = input("Enter your vehicle number (natural number): ")
+            if vehicle.isdigit() and int(vehicle) > 0:
+                vehicle = int(vehicle)
+                break
+            else:
+                print("Enter valid number only")
+
         for j in range(len(parking)):
-            if parking[j] == vech:
-                print("The vehicle is free from the slot.", j + 1)
+            if parking[j] == vehicle:
+                print("Vehicle removed from slot:", j + 1)
                 parking[j] = 0
                 break
         else:
-            print("You did not park the vehicle.")
-
+            print("This vehicle is not parked here.")
 
     else:
-        print("Please Enter a valid input.")
-
-
-
-
-
+        print("Please enter a valid input.")
 
 
 
